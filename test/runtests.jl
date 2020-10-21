a1523f6c2dade147a5db9ba8afccff1addf0a93e
Niclas Popp <niclas.popp@tum.de>
2020-10-22 00:10:03 +0200

    increased code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,21 +29,26 @@ Testdata = rand(d1,points)
     dr = MultivariateDiscretization.DoaneRule(Testdata,dims)
     cpd = MultivariateDiscretization.CPD_clustered(Testdata,dims,points,3,float(3))
     ipd = MultivariateDiscretization.greedy_IPD_clustered(Testdata,dims,3,3)
+    uw = MultivariateDiscretization.greedy_IPD_clustered(Testdata,dims,3)
 
     @test typeof(ipd) == DataFrame
     @test typeof(cpd) == DataFrame
     @test typeof(bb) == DataFrame
     @test typeof(dr) == DataFrame
+    @test typeof(uw) == DataFrame
 
     cjs_ipd = MultivariateDiscretization.CJS_empirical(Matrix(float.(ipd)),dims,points,200)
     cjs_cpd = MultivariateDiscretization.CJS_empirical(Matrix(float.(cpd)),dims,points,200)
     cjs_bb = MultivariateDiscretization.CJS_empirical(Matrix(float.(bb)),dims,points,200)
     cjs_dr = MultivariateDiscretization.CJS_empirical(Matrix(float.(dr)),dims,points,200)
+    cjs_uw = MultivariateDiscretization.CJS_empirical(Matrix(float.(uw)),dims,points,200)
     cjs_or = MultivariateDiscretization.CJS_empirical(Testdata,dims,points,200)
 
     @test typeof(cjs_ipd) == Array{Float64,2}
     @test typeof(cjs_cpd) == Array{Float64,2}
     @test typeof(cjs_bb) == Array{Float64,2}
     @test typeof(cjs_dr) == Array{Float64,2}
+    @test typeof(cjs_uw) == Array{Float64,2}
+    @test typeof(cjs_or) == Array{Float64,2}
 
 end
